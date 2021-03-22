5602f2e80b2d85917b4c687567d5a9001afb12fd
다윤님 <80930697+quntnim@users.noreply.github.com>
2021-03-22 10:16:59 +0900

    Create c 20200312.c

---
+++ c 20200312.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+/*
+int main()
+{
+	printf("\"Hello World!\"");
+	return 0;
+}
+*/
+
+/*
+int main()
+{
+	printf("내\n이\n름");
+	return 0;
+}
+*/
+
+/*
+int main()
+{
+	printf("안녕!!\n내 이름은 : 홍길동이야.\n나는 17살이야.\n나의 좌우명은 \"최선을 다해 노력하자\"");
+	return 0;
+}
+*/
+
+/*
+int main()
+{
+	printf("안녕!!\n");
+	printf("내 이름은 : 홍길동이야.\n");
+	printf("나는 17살이야.\n");
+	printf("나의 좌우명은 \"최선을 다해 노력하자\"");
+	return 0;
+}
+*/
+
+main() 
+{
+	char d;
+	scanf("%c", &d);
+	printf("%c", d);
+	return 0;
+}
